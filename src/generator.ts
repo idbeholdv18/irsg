0e8eb32c6fa2ca0062cb2344200767f5ec983b2f
idbeholdv18 <alimsadullaev18@gmail.com>
2025-03-01 23:56:23 +0300

    templates fix and generator update

--- src/generator.ts
+++ src/generator.ts
@@ -1,35 +1,60 @@
 import fs from "fs-extra";
 import path from "path";
 import ejs from "ejs";
+import { Layer } from "./layers/Layer.enum";
+import { toPascalCase } from "./utils/toPascalCase";
+import chalk from "chalk";
 
 const TEMPLATES_DIR = path.join(__dirname, "..", "templates", "slice");
 
-export const generateSlice = async (sliceName: string) => {
-  const sliceDir = path.join(process.cwd(), "src", "slices", sliceName);
+export const generateSlice = async (
+  layer: Layer,
+  name: string,
+  opt?: string
+) => {
+  const sliceDir = path.join(process.cwd(), "src", layer, name);
+  if (await fs.pathExists(sliceDir)) {
+    console.error(
+      chalk.red(
+        `Error: slice with name '${name}' already exists on layer '${layer}'.`
+      )
+    );
+    process.exit(1);
+  }
 
   try {
     await fs.ensureDir(sliceDir);
-    await fs.ensureDir(path.join(sliceDir, "components"));
 
     const indexTemplate = await fs.readFile(
       path.join(TEMPLATES_DIR, "index.ts.ejs"),
       "utf-8"
     );
-    const indexContent = ejs.render(indexTemplate, { sliceName });
+    const indexContent = ejs.render(indexTemplate, {
+      name: toPascalCase(name),
+      opt,
+    });
     await fs.writeFile(path.join(sliceDir, "index.ts"), indexContent);
 
     const componentTemplate = await fs.readFile(
       path.join(TEMPLATES_DIR, "component.tsx.ejs"),
       "utf-8"
     );
-    const componentContent = ejs.render(componentTemplate, { sliceName });
+    const componentDir = path.join(sliceDir, "ui");
+    await fs.ensureDir(componentDir);
+    const componentContent = ejs.render(componentTemplate, {
+      name: toPascalCase(name),
+    });
     await fs.writeFile(
-      path.join(sliceDir, "components", `${sliceName}Component.tsx`),
+      path.join(componentDir, `${toPascalCase(name)}.tsx`),
       componentContent
     );
 
-    console.log(`Слайс '${sliceName}' успешно создан!`);
+    console.log(
+      chalk.green(
+        `OK: Slice '${name}' layer '${layer}' has been successfully created`
+      )
+    );
   } catch (error) {
-    console.error("Ошибка при создании слайса:", error);
+    console.error(chalk.red("Error: Slice creating error:", error));
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 #!/usr/bin/env node
+import pkg from "../package.json";
 
+import chalk from "chalk";
 import { Command } from "commander";
 import { generateSlice } from "./generator";
+import { Layer } from "./layers/Layer.enum";
+import { chooseLayer } from "./inquirer/chooseLayer";
+import { chooseName } from "./inquirer/chooseName";
 
 const program = new Command();
 
 program
-  .version("1.0.0")
-  .description("CLI генератор для создания слайсов")
-  .command("generate:slice <name>")
-  .description("Создать новый слайс")
-  .action((name) => {
-    generateSlice(name);
+  .name("irsg")
+  .description("CLI generator for FSD slices creating")
+  .version(pkg.version);
+
+program
+  .command("g")
+  .description("Creates new slice")
+  .action(async (options) => {
+    const layer = await chooseLayer();
+    const name = await chooseName();
+
+    generateSlice(layer as Layer, name, options.opt);
+  })
+  .configureOutput({
+    writeErr: (str) => process.stdout.write(chalk.red(`${str}`)),
   });
 
 program.parse(process.argv);
