31cea12df7287d8e5a65f146b9f0d81febeef27b
idbeholdv18 <alimsadullaev18@gmail.com>
2025-03-02 16:56:14 +0300

    Project name prompt fn

---
+++ src/cra/inquirer/promptProjectName.ts
@@ -0,0 +1,34 @@
+import chalk from "chalk";
+import { existsSync } from "fs";
+import inquirer from "inquirer";
+import path from "path";
+
+export async function promptProjectName() {
+  while (true) {
+    try {
+      const { projectName } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "projectName",
+          message: "Enter project name:",
+          validate: (input) => {
+            if (!input.trim()) return "Error: Project name can't be empty";
+            if (existsSync(path.join(process.cwd(), input)))
+              return "Directory already exists. Try another name";
+            return true;
+          },
+        },
+      ]);
+      if (
+        projectName.trim() &&
+        !existsSync(path.join(process.cwd(), projectName))
+      ) {
+        return projectName;
+      }
+    } catch (e) {
+      // Обработа ошибки, если пользователь закрыл prompt
+      console.log(chalk.red("\nStopped (Ctrl+C)"));
+      process.exit(0);
+    }
+  }
+}
